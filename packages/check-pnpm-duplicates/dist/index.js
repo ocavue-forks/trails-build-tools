2b3b20eb1ccdc2884fefa5f20cc9d7b43a318ce3
ocavue <ocavue@users.noreply.github.com>
2026-02-04 07:38:22 +0000

    beta 3

--- packages/check-pnpm-duplicates/dist/index.js
+++ packages/check-pnpm-duplicates/dist/index.js
@@ -61,7 +61,7 @@ function typedKeys(object) {
 }
 
 // package.json
-var version = "0.2.7-beta.1";
+var version = "0.2.7-beta.3";
 
 // src/readConfig.ts
 import { load as loadYaml } from "js-yaml";
